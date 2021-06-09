83002f80ce652444943f16854658127b57b4b2da
reouen <69197425+reouen@users.noreply.github.com>
2021-06-09 18:13:09 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import {BrowserRouter as Router} from 'react-router-dom'
+
+
+function App() {
+  return (
+<Router>
+  <Navbar/>
+</Router>    
+    
+       
+  );
+};
+
+export default App;
